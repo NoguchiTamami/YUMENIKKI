28df8c6827d49f34d25f363cec484ede5b0fe414
NoguchiTamami <mikke417@gmail.com>
2020-07-02 13:00:10 +0900

    Bootstrapテンプレート適用

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-	def new
+ def new
    @posts = Post.all
    @post = Post.new
  end
@@ -16,34 +16,33 @@ class UsersController < ApplicationController
   @user = User.find(params[:id])
   @post = Post.new
   @posts = @user.posts
-end
+ end
 
-def edit
+ def edit
   @user = User.find(params[:id])
-end
+ end
 
-def update
- @user = User.find(params[:id])
- if @user.update(user_params)
-  redirect_to user_path(@user.id), notice: 'User was successfully created.'
-else
- render action: :edit
+  def update
+  @user = User.find(params[:id])
+  if @user.update(user_params)
+   redirect_to user_path(@user.id), notice: 'User was successfully created.'
+ else
+  render action: :edit
 end
-end
+ end
 
 def ensure_correct_user
 
- if current_user.id !=  params[:id].to_i
+  if current_user.id !=  params[:id].to_i
 
-   redirect_to user_path(current_user.id)
+    redirect_to user_path(current_user.id)
 
- end
+  end
 end
 
 private
 
 def user_params
   params.require(:user).permit()
-  
 end
 end
