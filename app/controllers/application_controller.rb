fe7503202691725d9a8f4d55a9848dc3f51590c0
NoguchiTamami <mikke417@gmail.com>
2020-07-07 11:54:58 +0900

    検索機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
 	 before_action :configure_permitted_parameters, if: :devise_controller?
      before_action :set_search
 
+def set_search
+  #@search = Article.search(params[:q])
+  @search = Post.ransack(params[:q]) #ransackメソッド推奨
+  @search_posts = @search.result.page(params[:page])
+end
+
 	def after_sign_in_path_for(resource)
     posts_path # ログイン後に遷移するpathを設定
     end
